7adff3ad71c6a2de9a2f28298701b17fdcc286ee
Ilia <mayksik@gmail.com>
2023-01-30 21:18:40 +0300

    Add HomeWork

---
+++ Lesson5/src/HomeWork/Calc/Core/MVP/Model.java
@@ -0,0 +1,15 @@
+package HomeWork.Calc.Core.MVP;
+/*
+Interface with definition of operations
+ */
+public interface Model<T> {
+
+    T sum(T obj);
+
+    T div(T obj);
+
+    T sub(T obj);
+
+    T mul(T obj);
+
+}
